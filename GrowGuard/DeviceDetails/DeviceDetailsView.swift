9b2570a1a203b590950a9ee32b0cff6b72be189f
Veit Progl <veit-progl@t-online.de>
2024-10-07 19:54:58 +0200

    Add SettingsView

--- GrowGuard/DeviceDetails/DeviceDetailsView.swift
+++ GrowGuard/DeviceDetails/DeviceDetailsView.swift
@@ -93,64 +93,7 @@ struct DeviceDetailsView: View {
             viewModel.device.optimalRange = optimalRange
             viewModel.saveDatabase()
         }) {
-            List {
-                Section(header: Text("Brigtness")) {
-                    HStack {
-                        Text("Min Brigtness")
-                        TextField("0", value: $optimalRange.minBrightness, format: .number)
-                            .keyboardType(.decimalPad)
-                    }
-                    
-                    HStack {
-                        Text("Max Brigtness")
-                        TextField("0", value: $optimalRange.maxBrightness, format: .number)
-                            .keyboardType(.decimalPad)
-                    }
-                }
-
-                Section(header: Text("Temperature")) {
-                    HStack {
-                        Text("Min Temperature")
-                        TextField("0", value: $optimalRange.minTemperature, format: .number)
-                            .keyboardType(.decimalPad)
-                    }
-                    
-                    HStack {
-                        Text("Max Temperature")
-                        TextField("0", value: $optimalRange.maxTemperature, format: .number)
-                            .keyboardType(.decimalPad)
-                    }
-                }
-                
-                
-                Section(header: Text("Moisture")) {
-                    HStack {
-                        Text("Min Moisture")
-                        TextField("0", value: $optimalRange.minMoisture, format: .number)
-                            .keyboardType(.decimalPad)
-                    }
-                    
-                    HStack {
-                        Text("Max Moisture")
-                        TextField("0", value: $optimalRange.maxMoisture, format: .number)
-                            .keyboardType(.decimalPad)
-                    }
-                }
-
-                Section(header: Text("Conductivity")) {
-                    HStack {
-                        Text("Min Conductivity")
-                        TextField("0", value: $optimalRange.minConductivity, format: .number)
-                            .keyboardType(.decimalPad)
-                    }
-                    
-                    HStack {
-                        Text("Max Conductivity")
-                        TextField("0", value: $optimalRange.maxConductivity, format: .number)
-                            .keyboardType(.decimalPad)
-                    }
-                }
-            }
+            SettingsView(optimalRange: $optimalRange)
         }
     }
 }
