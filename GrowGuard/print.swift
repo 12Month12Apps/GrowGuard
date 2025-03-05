97109a5cf0516351069188c60ac30a41960b0cc8
Veit Progl <veit-progl@t-online.de>
2025-03-05 21:37:37 +0100

    fix shortcut performance

---
+++ GrowGuard/print.swift
@@ -0,0 +1,20 @@
+//
+//  print.swift
+//  GrowGuard
+//
+//  Created by veitprogl on 05.03.25.
+//
+
+public func print(_ object: Any...) {
+    #if DEBUG
+    for item in object {
+        Swift.print(item)
+    }
+    #endif
+}
+
+public func print(_ object: Any) {
+    #if DEBUG
+    Swift.print(object)
+    #endif
+}
